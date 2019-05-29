1bfd7e2af36e9ecb40aa72f7026513a38d0a674e
Pagani Walter <paganiwalter@gmail.com>
2019-05-29 03:35:50 -0300

    Modificando admin.py de anuncios
    Se registraron los modelos dentro del administrador, para que los
    mismos sean visibles y pueden tener un mejor aspecto que el que
    normalmente se tiene por defecto.

--- apps/anuncios/admin.py
+++ apps/anuncios/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from .models import Categoria, Novedad, Lugar, Evento, Faq
 
-# Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'nombre',
+		'activo'
+	)
+
+
+@admin.register(Novedad)
+class NovedadAdmin(admin.ModelAdmin):
+	list_display = (
+		'autor',
+		'creada',
+		'modificada',
+		'titulo',
+		'enlace',
+		'activo'
+	)
+
+
+@admin.register(Lugar)
+class LugarAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'nombre',
+		'activo'
+	)
+
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+	list_display = (
+		'fecha_inicio',
+		'fecha_fin',
+		'hora_inicio',
+		'hora_fin',
+		'titulo',
+		'lugar',
+		'enlace',
+		'disertantes',
+		'autor',
+		'activo'
+	)
+
+
+@admin.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+	list_display = (
+		'titulo',
+		'creada',
+		'modificada',
+		'slug',
+		'activo'
+	)
