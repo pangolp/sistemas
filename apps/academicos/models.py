73ad5f412961c28161dbd029651ff7e0306affc7
Pagani Walter <paganiwalter@gmail.com>
2019-05-31 04:51:10 -0300

    Agregando modelos a academico
    Se crearon los primeros modelos de academico.

--- apps/academicos/models.py
+++ apps/academicos/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Año(models.Model):
+	numero = models.IntegerField(unique=True, primary_key=True)
+	observación = RichTextField(blank=True, null=True)
+
+	def __str__(self):
+		return '%s' % (self.numero)
+
+	class Meta:
+		db_table = 'academico_año'
+		verbose_name_plural = 'años'
+		ordering = ['numero']
+
+
+class Materia(models.Model):
+	año = models.ForeignKey(Año, on_delete=models.CASCADE)
+	nombre = models.CharField(max_length=255, blank=False, null=False, unique=True)
+	profesores = RichTextField(blank=True, null=True)
+	observación = RichTextField(blank=True, null=True)
+	slug = models.SlugField(max_length=255, editable=False)
+
+	def __str__(self):
+		return '%s' % (self.nombre)
+
+	def save(self):
+		self.slug = slugify(self.nombre)
+		super(Materia, self).save()
+
+	class Meta:
+		db_table = 'academico_materia'
+		verbose_name_plural = 'materias'
+		ordering = ['año']
