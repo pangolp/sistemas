d09df7f7e4189bff6a3f43f023cb8df3eeeaf4ab
Pagani Walter <paganiwalter@gmail.com>
2019-06-25 01:09:09 -0300

    Se creo la planilla base.
    Se agregaron las urls a anuncios. Se modifico el view para crear la primera funcion,
    tambien se cargo el menu con las opciones en base y la especificaron las urls.

--- apps/anuncios/views.py
+++ apps/anuncios/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.views.generic.detail import DetailView
+from .models import Novedad
 
-# Create your views here.
+
+class IndexView(ListView):
+	queryset = Novedad.objects.filter(activo=True)
+	template_name = 'anuncios/novedad/novedades.html'
+	context_object_name = 'novedades'
+
+class NovedadView(DetailView):
+	template_name = 'anuncios/novedad/detalle_novedad.html'
+	model = Novedad
+	context_object_name = 'novedad'
